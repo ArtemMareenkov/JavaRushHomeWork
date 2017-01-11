32add82d489275d0b07b579522242847e452c862
ArtemMareenkov <artem.mareenkov@gmail.com>
2017-01-11 18:35:19 +0200

    Lesson2_Level8_DONE

---
+++ src/com/javarush/test/level02/lesson08/task04/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.test.level02.lesson08.task04;
+
+/* Минимум четырех чисел
+Написать функцию, которая вычисляет минимум из четырёх чисел.
+Функция min(a,b,c,d) должна использовать (вызывать) функцию min(a,b)
+Подсказка:
+Нужно написать тело обеих существующих функций min и исправить их возвращаемые значения.
+*/
+public class Solution
+{
+    public static int min(int a, int b, int c, int d)
+    {
+        int m = min(a,b);
+        if (m<c & m<d)
+            return m;
+        if (c<m & c<d)
+            return c;
+        else
+            return d;
+
+    }
+
+    public static int min(int a, int b)
+    {
+        int m2;
+        if (a<b)
+            m2=a;
+        else
+            m2=b;
+        return m2;
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println( min(-20, -10) );
+        System.out.println( min(-20, -10, -30, -40) );
+        System.out.println( min(-20, -10, -30, 40) );
+    }
+}
